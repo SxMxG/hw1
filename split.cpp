922db24847258bf2c7eaee688b8689061fbf60d4
Andrew Placencia <sxmxg01@gmail.com>
2024-01-23 12:04:44 -0800

    test1 for split

--- split.cpp
+++ split.cpp
@@ -18,6 +18,20 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  if(in == NULL){
+    return;
+  }
+  
+  if(in->value % 2 == 0){
+    evens = in;
+    evens = evens->next; 
+  }
+  else{
+    odds = in;
+    odds = odds->next;
+  }
+  split(in->next, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
