687ecef44b53139c68d1df8d937c4b5964904556
Andrew Placencia <sxmxg01@gmail.com>
2024-01-27 00:21:46 -0800

    small test boi

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -14,12 +14,7 @@ int main(int argc, char* argv[])
     dat.push_front("pls");
     dat.push_front("work");
     dat.push_front("work");
-    for(int i = 0;i<15;i++){
-        dat.push_front("added");
-    }
-    for(int i = 0;i<15;i++){
-        cout<< i <<dat.get(i)<<endl;
-    }
+    
     cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << " " << dat.get(3) << " " << dat.get(4) << " " << dat.get(5)<<endl;
     cout <<"do this even work"<<endl;
     // prints: 8 7 9
