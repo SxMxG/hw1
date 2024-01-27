e602274b5be7c2993c75f8783a7591ec1bed47a6
Andrew Placencia <sxmxg01@gmail.com>
2024-01-26 23:24:22 -0800

    a

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
     for(int i = 0;i<15;i++){
         cout<< i <<dat.get(i)<<endl;
     }
-    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << " " << dat.get(3) << " " << dat.get(4) << " " << dat.get(2)endl;
+    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << " " << dat.get(3) << " " << dat.get(4) << " " << dat.get(5)<<endl;
     cout <<"do this even work"<<endl;
     // prints: 8 7 9
     cout << dat.size() << endl;  // prints 3 since there are 3 strings stored
