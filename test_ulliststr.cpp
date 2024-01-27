70e1187c85de9e10555b35807abc1793aac63e5d
Andrew Placencia <sxmxg01@gmail.com>
2024-01-26 23:12:10 -0800

    testing

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -10,9 +10,9 @@
 int main(int argc, char* argv[])
 {
     ULListStr dat;
-    dat.push_back(7);
-    dat.push_front(8);
-    dat.push_back(9);
+    dat.push_back("pls");
+    dat.push_front("work");
+    dat.push_back("work");
     cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
     // prints: 8 7 9
     cout << dat.size() << end;  // prints 3 since there are 3 strings stored
