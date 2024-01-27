721376a1560c64235a962ab3d3a7a3af37af686c
Andrew Placencia <sxmxg01@gmail.com>
2024-01-26 23:23:28 -0800

    more testing :)

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -14,7 +14,13 @@ int main(int argc, char* argv[])
     dat.push_back("pls");
     dat.push_back("work");
     dat.push_back("work");
-    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
+    for(int i = 0;i<10;i++){
+        dat.push_back("bs");
+    }
+    for(int i = 0;i<15;i++){
+        cout<< i <<dat.get(i)<<endl;
+    }
+    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << " " << dat.get(3) << " " << dat.get(4) << << " " << dat.get(2)endl;
     cout <<"do this even work"<<endl;
     // prints: 8 7 9
     cout << dat.size() << endl;  // prints 3 since there are 3 strings stored
